5fbffd3d2200e7942228229ae7d841ac5accd34e
kim-yonghyeon <707ksb@gmail.com>
2025-03-08 22:17:03 +0900

    Fix: Add gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `weddingInvitation`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: ["gatsby-plugin-styled-components", `gatsby-plugin-netlify`],
 };
